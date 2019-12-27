962d105e73e70bb692b5752e71a2126b30f46b5f
Asilek <asilek@hotmail.com>
2019-12-27 15:01:15 -0300

    Modificación

--- src/test/java/com/selenium/prueba/TestRunner.java
+++ src/test/java/com/selenium/prueba/TestRunner.java
@@ -34,21 +34,21 @@ public class TestRunner {
 
 	@SuppressWarnings("static-access")
 	@Test
-	public void buscarConsola() {
+	public void buscarConsola()  {
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.cssSelector(".HamburgerIcon_hamburgerText__3_7OL")).click();
 		driver.findElement(By.cssSelector("#item-3 > .FirstLevelItems_menuText__UYB9A")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().wait(30);
 		driver.findElement(By.linkText("Consolas")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().wait(30);
 		System.out.println("Selecciono Consolas");
 		{
 			WebElement element = driver.findElement(By.cssSelector(".fb-huincha-link-03"));
 			Actions builder = new Actions(driver);
 			builder.moveToElement(element).perform();
 		}
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().wait(30);
 		{
 			WebElement element = driver.findElement(By.tagName("body"));
 			Actions builder = new Actions(driver);
@@ -69,31 +69,32 @@ public class TestRunner {
 		js.executeScript("window.scrollTo(0,600)");
 		driver.findElement(By.cssSelector("#testId-Accordion-Marca > .icon")).click();
 		driver.findElement(By.id("testId-Multiselect-Marca")).click();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().wait(30);
 		// driver.findElement(By.id("testId-Multiselect-Marca")).sendKeys("nintendo");
 		driver.findElement(
 				By.cssSelector(".jsx-4207689641:nth-child(3) .jsx-53718149:nth-child(12) .jsx-394545448:nth-child(2)"))
 				.click();
 		System.out.println("Filtro Nintendo");
+		driver.manage().wait(30);
+		driver.findElement(By.id("testId-Pod-action-8014967")).click();
 		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-		driver.findElement(By.id("testId-Pod-action-8275884")).click();
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector(".fb-quantity-input__plus")).click();
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector(".fb-quantity-input__plus")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector(".fb-quantity-input__plus")).click();
-
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector(".fb-quantity-input__plus")).click();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector(".fb-quantity-input__plus")).click();
-		js.executeScript("window.scrollTo(0,501)");
-
-		System.out.println("Agrego más");
+	    js.executeScript("window.scrollTo(0,188)");
+	    js.executeScript("window.scrollTo(0,206)");
+	    {
+	      WebElement element = driver.findElement(By.cssSelector(".fb-product-cta__controls--actions--choose"));
+	      Actions builder = new Actions(driver);
+	      builder.moveToElement(element).perform();
+	    }
+	    {
+	      WebElement element = driver.findElement(By.tagName("body"));
+	      Actions builder = new Actions(driver);
+	      builder.moveToElement(element, 0, 0).perform();
+	    }
+	    driver.findElement(By.cssSelector(".fb-quantity-input__plus")).click();
+	    driver.findElement(By.cssSelector(".fb-quantity-input__plus")).click();
 
 	}
-
+	
 	@AfterMethod()
 	public void teardownTest() {
 		// Close browser and end the session
